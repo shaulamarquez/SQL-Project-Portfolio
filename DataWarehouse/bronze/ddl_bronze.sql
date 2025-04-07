a20f34787576418e96d240f4f43dda8efc01ef5a
Shaula Marquez <154650520+shaulamarquez@users.noreply.github.com>
2025-04-07 19:23:56 +0800

    Update ddl_bronze.sql

--- DataWarehouse/bronze/ddl_bronze.sql
+++ DataWarehouse/bronze/ddl_bronze.sql
@@ -16,7 +16,7 @@ CREATE TABLE bronze.crm_cust_info (
 	cst_key NVARCHAR(50),
 	cst_firstname NVARCHAR(50),
 	cst_lastname NVARCHAR(50),
-	cst_material_status NVARCHAR(50),
+	cst_maarital_status NVARCHAR(50),
 	cst_gndr NVARCHAR(50),
 	cst_create_date DATE
 );
